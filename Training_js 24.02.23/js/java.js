7b3c4d11aa4d46e693ebf0f4d2bea2db6fdac94e
R2-D2-2 <a1998vk@yandex.ru>
2023-02-24 20:50:46 +0400

    Начало обучения JavaScript

---
+++ Training_js 24.02.23/js/java.js
@@ -0,0 +1,24 @@
+'use strict'; 
+//Небольшое задание
+//alert ("«Я JavaScript!»");
+//ctrl + / закоментировать кусочек кода
+
+//Переменные let с именем user... var почти тоже, что и let, но устаревший вариант.
+let user = 'John',
+    age = 25,
+    message = 'Hello';
+
+let hello = 'Hello world!';
+
+let message2;
+
+// копируем значение 'Hello world' из переменной hello в переменную message
+message2 = hello;
+
+//const неизменяемая переменная. Часто используют для заранее известных цветов. const COLOR_RED = "#F00"; Для упрощения записи.
+const num = '25';
+
+//Небольшое задание
+// let name = "Джон",
+//     admin = name;
+//     alert (admin);
