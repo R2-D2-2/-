7f7e67099744be1ac1a5173effdb9a6138f02810
R2-D2-2 <a1998vk@yandex.ru>
2023-03-07 22:15:06 +0400

    Возвращение блудного сына 2. 07.03.23

--- Training_js 24.02.23/js/java.js
+++ Training_js 24.02.23/js/java.js
@@ -553,3 +553,90 @@ typeof alert // "function"  (3)
 //     num = prompt("Введите число больше 100?", 0);
 //   } while (num <= 100 && num);
 
+// Конструкция "switch"
+
+// Конструкция switch заменяет собой сразу несколько if.
+
+// Конструкция switch имеет один или более блок case и необязательный блок default.
+
+// switch(x) {
+//     case 'value1':  // if (x === 'value1')
+//       ...
+//       [break]
+  
+//     case 'value2':  // if (x === 'value2')
+//       ...
+//       [break]
+  
+//     default:
+//       ...
+//       [break]
+//   }
+
+// Например:
+// let a = 2 + 2;
+
+// switch (a) {
+//   case 3:
+//     alert( 'Маловато' );
+//     break;
+//   case 4:
+//     alert( 'В точку!' );
+//     break;
+//   case 5:
+//     alert( 'Перебор' );
+//     break;
+//   default:
+//     alert( "Нет таких значений" );
+// }
+
+// Вариации:
+
+// let a = 3;
+
+// switch (a) {
+//   case 4:
+//     alert('Правильно!');
+//     break;
+
+//   case 3: // (*) группируем оба case
+//   case 5:
+//     alert('Неправильно!');
+//     alert("Может вам посетить урок математики?");
+//     break;
+
+//   default:
+//     alert('Результат выглядит странновато. Честно.');
+// }
+
+// Небольшое задание:
+
+// if(browser == 'Edge') {
+//     alert("You've got the Edge!");
+//   } else if (browser == 'Chrome'
+//    || browser == 'Firefox'
+//    || browser == 'Safari'
+//    || browser == 'Opera') {
+//     alert( 'Okay we support these browsers too' );
+//   } else {
+//     alert( 'We hope that this page looks ok!' );
+//   }
+
+// Небольшое задание:
+
+// const number = +prompt('Введите число между 0 и 3', '');
+
+// switch (number) {
+//   case 0:
+//     alert('Вы ввели число 0');
+//     break;
+
+//   case 1:
+//     alert('Вы ввели число 1');
+//     break;
+
+//   case 2:
+//   case 3:
+//     alert('Вы ввели число 2, а может и 3');
+//     break;
+// }
